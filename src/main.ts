4ee7a8917e919932744ca247de8f953d4101aa68
antonioace <antonioac@ufps.edu.co>
2022-07-27 18:21:06 -0500

    Primeros cambios

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (!navigator.geolocation) {
+  throw new Error('Navegador no soportado');
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
